24b45632a9cc47f4989bee6324b247450f81075d
macg806 <mauricio.capiz@gmail.com>
2026-04-09 13:35:20 -0600

    Dockerfile location fixed

--- app/metrics.py
+++
@@ -1,4 +0,0 @@
-from prometheus_client import Counter, Histogram
-
-REQUEST_COUNT = Counter('request_count', 'Total API Requests')
-REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency')
